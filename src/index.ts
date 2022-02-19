d715e75380b9bcccf7f40b61296e405c6b890b02
ushieru <uzielcocolan@gmail.com>
2022-02-18 18:47:52 -0600

    delete console.log

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 import express, { Response, Request, NextFunction } from "express";
 import knex from "knex";
 import { User } from "./models/User";
-console.log(process.env.DATABASE_URL)
+
 const PORT = process.env.PORT || 3030
 const server = express()
 const database = knex({
